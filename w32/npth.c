e369dd721762262fd0f85361b348207ced13c6b2
NIIBE Yutaka <gniibe@fsij.org>
2024-11-12 14:03:53 +0900

    w32: Fix npth_cond_signal and npth_cond_broadcast.
    * w32/npth.c (struct dl): New struct.
    (struct npth_impl_s): Have the struct dl at top.
    (dequeue_thread, enqueue_thread): Replace old implementation.
    (new_thread): Fix for the double linked list.
    (free_thread): Likewise.
    (struct npth_cond_s): Have the struct dl.
    (npth_cond_init, npth_cond_destroy): Fix for the double linked list.
    (npth_cond_signal, npth_cond_broadcast): Fix to update the list
    correctly.
    (npth_cond_wait, npth_cond_timedwait): Fix for the double linked list.
    
    --
    
    GnuPG-bug-id: 7386
    Signed-off-by: NIIBE Yutaka <gniibe@fsij.org>

--- w32/npth.c
+++ w32/npth.c
@@ -172,8 +172,19 @@ leave_npth (const char *function)
 #define LEAVE() leave_npth(__FUNCTION__)
 
 
+struct dl {
+  struct dl *next;
+  struct dl *prev;
+};
+
 struct npth_impl_s
 {
+  /* Doubly-linked list for the waiter queue in condition
+     variables.  */
+  /* Should be the first member of struct npth_impl_s, so that
+   * casting from the address of l can be the head of struct npth_impl_s.  */
+  struct dl l;
+
   /* Usually there is one ref owned by the thread as long as it is
      running, and one ref for everybody else as long as the thread is
      joinable.  */
@@ -190,11 +201,6 @@ struct npth_impl_s
 
   char name[THREAD_NAME_MAX + 1];
 
-  /* Doubly-linked list for the waiter queue in condition
-     variables.  */
-  npth_impl_t next;
-  npth_impl_t *prev_ptr;
-
   /* The event on which this thread waits when it is queued.  */
   HANDLE event;
 
@@ -203,32 +209,35 @@ struct npth_impl_s
 
 
 static void
-dequeue_thread (npth_impl_t thread)
+dequeue_thread (struct dl *l)
 {
-  /* Unlink the thread from any condition waiter queue.  */
-  if (thread->next)
+  /* Unlink the entry of L from a condition waiter queue.  */
+  struct dl *next = l->next;
+  struct dl *prev = l->prev;
+
+  if (next)
     {
-      thread->next->prev_ptr = thread->prev_ptr;
-      thread->next = NULL;
+      next->prev = prev;
+      l->next = NULL;
     }
-  if (thread->prev_ptr)
+
+  if (prev)
     {
-      *(thread->prev_ptr) = thread->next;
-      thread->prev_ptr = NULL;
+      prev->next = next;
+      l->prev = NULL;
     }
 }
 
 
-/* Enqueue THREAD to come after the thread whose next pointer is
-   prev_ptr.  */
 static void
-enqueue_thread (npth_impl_t thread, npth_impl_t *prev_ptr)
+enqueue_thread (struct dl *l, struct dl *head)
 {
-  if (*prev_ptr)
-    (*prev_ptr)->prev_ptr = &thread->next;
-  thread->prev_ptr = prev_ptr;
-  thread->next = *prev_ptr;
-  *prev_ptr = thread;
+  struct dl *last_entry = head->prev;
+
+  head->prev = l;
+  l->prev = last_entry;
+  l->next = head;
+  last_entry->next = l;
 }
 
 
@@ -268,8 +277,8 @@ new_thread (npth_t *thread_id)
   thread->detached = 0;
   thread->start_routine = NULL;
   thread->start_arg = NULL;
-  thread->next = NULL;
-  thread->prev_ptr = NULL;
+  thread->l.next = NULL;
+  thread->l.prev = NULL;
   /* We create the event when it is first needed (not all threads wait
      on conditions).  */
   thread->event = NULL;
@@ -294,7 +303,7 @@ free_thread (npth_t thread_id)
     CloseHandle (thread->event);
 
   /* Unlink the thread from any condition waiter queue.  */
-  dequeue_thread (thread);
+  dequeue_thread (&thread->l);
 
   free (thread);
 
@@ -968,7 +977,7 @@ struct npth_cond_s
      simple.  */
 
   /* The waiter queue.  */
-  npth_impl_t waiter;
+  struct dl waiter;
 };
 
 
@@ -986,7 +995,7 @@ npth_cond_init (npth_cond_t *cond_r,
   if (!cond)
     return errno;
 
-  cond->waiter = NULL;
+  cond->waiter.next = cond->waiter.prev = &cond->waiter;
 
   *cond_r = cond;
   return 0;
@@ -999,7 +1008,7 @@ npth_cond_destroy (npth_cond_t *cond)
   if (*cond == 0)
     return EINVAL;
 
-  if ((*cond)->waiter)
+  if ((*cond)->waiter.next != &(*cond)->waiter)
     return EBUSY;
 
   free (*cond);
@@ -1037,18 +1046,20 @@ npth_cond_signal (npth_cond_t *cond)
   int err;
   npth_impl_t thread;
   DWORD res;
+  struct dl *waiter;
 
   /* While we are protected, let's check for a static initializer.  */
   err = cond_init_check (cond);
   if (err)
     return err;
 
-  if ((*cond)->waiter == NULL)
+  waiter = (*cond)->waiter.next;
+  if (waiter == &(*cond)->waiter)
     return 0;
 
   /* Dequeue the first thread and wake it up.  */
-  thread = (*cond)->waiter;
-  dequeue_thread (thread);
+  dequeue_thread (waiter);
+  thread = (struct npth_impl_s *)waiter;
 
   res = SetEvent (thread->event);
   if (res == 0)
@@ -1075,25 +1086,26 @@ npth_cond_broadcast (npth_cond_t *cond)
   int err;
   npth_impl_t thread;
   DWORD res;
+  struct dl *waiter;
 
   /* While we are protected, let's check for a static initializer.  */
   err = cond_init_check (cond);
   if (err)
     return err;
 
-  if ((*cond)->waiter == NULL)
-    return 0;
-
-  while ((*cond)->waiter)
+  waiter = (*cond)->waiter.next;
+  while (waiter != &(*cond)->waiter)
     {
       /* Dequeue the first thread and wake it up.  */
-      thread = (*cond)->waiter;
-      dequeue_thread (thread);
+      dequeue_thread (waiter);
+      thread = (struct npth_impl_s *)waiter;
 
       res = SetEvent (thread->event);
       if (res == 0)
 	/* FIXME: An error here implies a mistake in the npth code.  Log it.  */
 	;
+
+      waiter = (*cond)->waiter.next;
     }
 
   /* Force the woken up threads into the mutex lock function (for the
@@ -1123,7 +1135,6 @@ npth_cond_wait (npth_cond_t *cond, npth_mutex_t *mutex)
   int err2;
   BOOL bres;
   npth_impl_t thread;
-  npth_impl_t *prev_ptr;
 
   /* While we are protected, let's check for a static initializer.  */
   err = cond_init_check (cond);
@@ -1142,11 +1153,7 @@ npth_cond_wait (npth_cond_t *cond, npth_mutex_t *mutex)
 	return map_error (GetLastError());
     }
 
-  /* Find end of queue and enqueue the thread.  */
-  prev_ptr = &(*cond)->waiter;
-  while (*prev_ptr)
-    prev_ptr = &(*prev_ptr)->next;
-  enqueue_thread (thread, prev_ptr);
+  enqueue_thread (&thread->l, &(*cond)->waiter);
 
   /* Make sure the event is not signaled before releasing the mutex.  */
   bres = ResetEvent (thread->event);
@@ -1159,7 +1166,7 @@ npth_cond_wait (npth_cond_t *cond, npth_mutex_t *mutex)
       err = npth_mutex_unlock (mutex);
       if (err)
 	{
-	  dequeue_thread (thread);
+	  dequeue_thread (&thread->l);
 	  return err;
 	}
     }
@@ -1169,7 +1176,7 @@ npth_cond_wait (npth_cond_t *cond, npth_mutex_t *mutex)
   LEAVE();
 
   /* Make sure the thread is dequeued (in case of error).  */
-  dequeue_thread (thread);
+  dequeue_thread (&thread->l);
 
   if (mutex)
     {
@@ -1194,7 +1201,6 @@ npth_cond_timedwait (npth_cond_t *cond, npth_mutex_t *mutex,
   int err2;
   BOOL bres;
   npth_impl_t thread;
-  npth_impl_t *prev_ptr;
   DWORD msecs;
 
   err = calculate_timeout (abstime, &msecs);
@@ -1240,16 +1246,12 @@ npth_cond_timedwait (npth_cond_t *cond, npth_mutex_t *mutex,
     /* Log an error.  */
     ;
 
-  /* Find end of queue and enqueue the thread.  */
-  prev_ptr = &(*cond)->waiter;
-  while (*prev_ptr)
-    prev_ptr = &(*prev_ptr)->next;
-  enqueue_thread (thread, prev_ptr);
+  enqueue_thread (&thread->l, &(*cond)->waiter);
 
   err = npth_mutex_unlock (mutex);
   if (err)
     {
-      dequeue_thread (thread);
+      dequeue_thread (&thread->l);
       return err;
     }
 
